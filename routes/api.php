1d9978b4a4e622cb4213f5c0256a1e59a932097d
wafaaezzat <wafaaezzat45@icloud.com>
2022-08-18 13:57:03 +0200

    create tokens

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\InvoiceController;
 use App\Http\Controllers\Api\V1\CustomerController;
+use GuzzleHttp\Middleware;
+use Laravel\Sanctum\Sanctum;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix'=>'v1','namespace'=>'App\Http\Controllers\Api\V1'],function(){
+Route::group(['prefix'=>'v1','namespace'=>'App\Http\Controllers\Api\V1','middleware'=>'auth:sanctum'],function(){
 
 Route::apiResource('customers',CustomerController::class);
 Route::apiResource('invoices',InvoiceController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function () {
+   $credintials=[
+    'email'=>'admin@icloud.com',
+    'password'=>'password'
+   ];
+
+
+   if(!Auth::attempt($credintials)){
+    $user=new User();
+    $user->name='Admin';
+    $user->email=$credintials['email'];
+    $user->password=Hash::make($credintials['password']);
+
+    $user->save();
+   }
+
+
+
+   if(Auth::attempt($credintials)){
+
+    $user=Auth::user();
+    $adminToken=$user->createToken('admin-token',['create','update','delete']);
+    $updateToken=$user->createToken('update-token',['create','update']); 
+    $basicToken=$user->createToken('basic-token');
+    
+
+    return[
+        'admin'=>$adminToken->plainTextToken,
+        'update'=>$updateToken->plainTextToken,
+        'basic'=>$basicToken->plainTextToken
+    ];
+
+
+   }
 });
